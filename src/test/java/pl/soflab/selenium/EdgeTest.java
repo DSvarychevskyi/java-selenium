b2b0eecfdcce41f95af27678be7a001669c586d1
Dmytro Svarychevskyi <Dmytro.Svarychevskyi@soflab.pl>
2018-12-11 11:49:30 +0100

    add logj4

--- src/test/java/pl/soflab/selenium/EdgeTest.java
+++ src/test/java/pl/soflab/selenium/EdgeTest.java
@@ -1,5 +1,6 @@
 package pl.soflab.selenium;
 
+import org.apache.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +12,7 @@ import java.util.concurrent.TimeUnit;
 
 public class EdgeTest {
 
+  static Logger log = Logger.getLogger(FirefoxTest.class);
   private static WebDriver driver;
 
   @BeforeAll
@@ -20,7 +22,7 @@ public class EdgeTest {
 
   @BeforeEach
   void setUpEach() {
-    System.out.println("Start Test");
+    log.info("Start Test");
     driver = new EdgeDriver();
     driver.manage().window().maximize();
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -34,6 +36,6 @@ public class EdgeTest {
   @AfterEach
   void setDownEach() {
     driver.quit();
-    System.out.println("End Test");
+    log.info("End Test");
   }
 }
